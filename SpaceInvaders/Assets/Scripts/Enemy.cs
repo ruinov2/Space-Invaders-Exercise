fbebdf95b4dfce0dbcb967779bdbcdbeb168fcae
ruinov2 <ruinov2@gmail.com>
2019-10-23 20:23:57 +0200

    Añadido Enemy
    Se crea el script de cambio de sprite a explosión

---
+++ SpaceInvaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Sprite enemy;
+    public Sprite explosion;
+    private float time = 0.25F; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        this.GetComponent<SpriteRenderer>().sprite = explosion;
+        Destroy(gameObject, time);
+    }
+}
